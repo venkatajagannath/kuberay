362caae8919c30e0b8db8bd0ff1836c690bcd6bd
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-03-26 16:52:16 -0400

    removed sh due to poor logging

--- dags/run_ray_job.py
+++ dags/run_ray_job.py
@@ -33,7 +33,6 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import os
-import sh
 
 from airflow.models.connection import Connection
 
@@ -62,9 +61,9 @@ dag = DAG(
     schedule_interval='@daily',
 )
 
-"""eksctl_create_cluster = BashOperator(
+eksctl_create_cluster = BashOperator(
     task_id='create_eks_cluster',
-    bash_command="
+    bash_command="""
         eksctl create cluster \
         --name RayCluster \
         --region us-east-2 \
@@ -73,84 +72,34 @@ dag = DAG(
         --nodes-min 1 \
         --nodes-max 3 \
        --managed
-    ",
-    dag=dag,
-)"""
-
-def create_eks_cluster():
-    sh.eksctl("create", "cluster", 
-              "--name", "RayCluster", 
-              "--region", "us-east-2", 
-              "--node-type", "m5.2xlarge", 
-              "--nodes", "2", 
-              "--nodes-min", "1", 
-              "--nodes-max", "3", 
-              "--managed")
-
-eksctl_create_cluster = PythonOperator(
-    task_id='create_eks_cluster',
-    python_callable=create_eks_cluster,
+    """,
     dag=dag,
 )
-
-def generate_kubeconfig():
-    sh.eksctl("utils", "write-kubeconfig", "--cluster=RayCluster", "--region=us-east-2")
-
 
 # Task to generate kubeconfig
-"""generate_kubeconfig = BashOperator(
+generate_kubeconfig = BashOperator(
         task_id='generate_kubeconfig',
-        bash_command=f"
+        bash_command=f"""
         eksctl utils write-kubeconfig --cluster=RayCluster --region=us-east-2
-        ",
+        """,
         dag = dag,
-    )"""
-
-generate_kubeconfig = PythonOperator(
-    task_id='generate_kubeconfig',
-    python_callable=generate_kubeconfig,
-    dag=dag,
-)
-
-def helm_install():
-    sh.helm("repo", "add", "kuberay", "https://ray-project.github.io/kuberay-helm/")
-    sh.helm("repo", "update")
-    sh.helm("install", "kuberay-operator", "kuberay/kuberay-operator", "--version", "1.0.0", "--create-namespace")
-
+    )
 
 # Task to install using Helm
-"""helm_install = BashOperator(
+helm_install = BashOperator(
     task_id='helm_install',
-    bash_command=f"
+    bash_command=f"""
     helm repo add kuberay https://ray-project.github.io/kuberay-helm/&&
     helm repo update &&
     helm install kuberay-operator kuberay/kuberay-operator --version 1.0.0 --create-namespace
-    ",
+    """,
     dag = dag,
-)"""
-
-helm_install = PythonOperator(
-    task_id='helm_install',
-    python_callable=helm_install,
-    dag=dag,
 )
 
 apply_ray_cluster_spec = BashOperator(
     task_id='apply_ray_cluster_spec',
     bash_command='kubectl apply -f /usr/local/airflow/dags/ray.yaml',
     dag=dag,
-)
-
-list_namespaces = BashOperator(
-    task_id = "list_namespaces",
-    bash_command = 'kubectl get namespaces',
-    dag = dag,
-)
-
-port_forward = BashOperator(
-    task_id = "port_forwarding",
-    bash_command = 'kubectl port-forward svc/RayCluster 8265:8265 10001:10001 &',
-    dag = dag,
 )
 
 #eksctl_delete_cluster = BashOperator(
@@ -161,5 +110,5 @@ port_forward = BashOperator(
 #    dag=dag,
 #)
 
-eksctl_create_cluster >> generate_kubeconfig >> helm_install >> apply_ray_cluster_spec >> list_namespaces >> port_forward
+eksctl_create_cluster >> generate_kubeconfig >> helm_install >> apply_ray_cluster_spec
 
