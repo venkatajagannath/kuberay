9fc4a64ef901625a16816638e48c73424defccf5
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-03-25 22:18:44 -0400

    namespace added

--- dags/run_ray_job.py
+++ dags/run_ray_job.py
@@ -130,7 +130,7 @@ helm_install = BashOperator(
     bash_command=f"""
     helm repo add kuberay https://ray-project.github.io/kuberay-helm/&&
     helm repo update &&
-    helm install kuberay-operator kuberay/kuberay-operator --version 1.0.0
+    helm install kuberay-operator kuberay/kuberay-operator --version 1.0.0 --create-namespace
     """,
     dag = dag,
 )
