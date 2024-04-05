fc11e9c3ddd696c59b7ec03e88ccd3c66637be92
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-04-04 23:25:30 -0400

    retry time changed

--- dags/start_ray_cluster.py
+++ dags/start_ray_cluster.py
@@ -21,7 +21,7 @@ default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 3, 26),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
 }
 
 CLUSTERNAME = 'RayCluster'
