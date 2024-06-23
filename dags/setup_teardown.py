6251524c4281bb531fac24af31c070900331f9e1
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-06-22 20:30:32 -0400

    Updated

--- dags/setup_teardown.py
+++ dags/setup_teardown.py
@@ -8,7 +8,7 @@ default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 3, 26),
     'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retry_delay': timedelta(minutes=0),
 }
 
 CLUSTERNAME = 'RayCluster'
