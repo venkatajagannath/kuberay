005d40eed5862108360a5288adad7f7eb98179f5
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-06-26 12:24:43 -0400

    Code and package updated

--- dags/setup_teardown.py
+++ dags/setup_teardown.py
@@ -42,12 +42,14 @@ submit_ray_job = SubmitRayJob(task_id="SubmitRayJob",
                               resources={},
                               dag = dag,)
 
-"""delete_cluster = DeleteRayCluster(task_id="DeleteRayCluster",
-                                  cluster_name=CLUSTERNAME,
-                                  dag = dag,)"""
+delete_cluster = DeleteRayCluster(task_id="DeleteRayCluster",
+                                  conn_id = "ray_conn",
+                                 ray_cluster_yaml=RAY_SPEC,
+                                 ray_svc_yaml= RAY_SVC,
+                                 use_gpu=False,
+                                 dag = dag,)
 
-setup_cluster >> submit_ray_job
 # Create ray cluster and submit ray job 
-"""setup_cluster.as_setup() >> submit_ray_job >> delete_cluster.as_teardown()
+setup_cluster.as_setup() >> submit_ray_job >> delete_cluster.as_teardown()
 setup_cluster >> delete_cluster
-"""
+
