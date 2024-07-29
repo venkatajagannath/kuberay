758024323958bbe49ddedf6502945ea86b94e94f
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-07-29 16:28:40 -0400

    conn_id added

--- dags/setup_teardown.py
+++ dags/setup_teardown.py
@@ -1,5 +1,3 @@
-
-import os
 from airflow import DAG
 from datetime import datetime, timedelta
 from ray_provider.operators.ray import SetupRayCluster, DeleteRayCluster,SubmitRayJob
@@ -11,6 +9,7 @@ default_args = {
     'retry_delay': timedelta(minutes=0),
 }
 
+CONN_ID = "ray_az"
 RAY_SPEC = '/usr/local/airflow/dags/scripts/ray.yaml'
 RAY_RUNTIME_ENV = {"working_dir": '/usr/local/airflow/dags/ray_scripts'}
 
@@ -22,14 +21,14 @@ dag = DAG(
 )
 
 setup_cluster = SetupRayCluster(task_id="SetupRayCluster",
-                                conn_id = "ray_conn",
+                                conn_id = CONN_ID,
                                 ray_cluster_yaml=RAY_SPEC,
                                 use_gpu=False,
                                 update_if_exists=False,
                                 dag = dag,)
 
 submit_ray_job = SubmitRayJob(task_id="SubmitRayJob",
-                              conn_id="ray_conn",
+                              conn_id=CONN_ID,
                               entrypoint='python script.py',
                               runtime_env= RAY_RUNTIME_ENV,
                               num_cpus=1,
@@ -43,15 +42,15 @@ submit_ray_job = SubmitRayJob(task_id="SubmitRayJob",
                               poll_interval=5,
                               dag = dag,)
 
-"""delete_cluster = DeleteRayCluster(task_id="DeleteRayCluster",
-                                  conn_id = "ray_conn",
+delete_cluster = DeleteRayCluster(task_id="DeleteRayCluster",
+                                  conn_id = CONN_ID,
                                  ray_cluster_yaml=RAY_SPEC,
                                  use_gpu=False,
-                                 dag = dag,)"""
-setup_cluster >> submit_ray_job
+                                 dag = dag,)
+# setup_cluster >> submit_ray_job
 # Create ray cluster and submit ray job 
-#setup_cluster.as_setup() >> submit_ray_job >> delete_cluster.as_teardown()
-#setup_cluster >> delete_cluster
+setup_cluster.as_setup() >> submit_ray_job >> delete_cluster.as_teardown()
+setup_cluster >> delete_cluster
 
 
 
