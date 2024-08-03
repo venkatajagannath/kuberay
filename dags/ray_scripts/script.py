c9adb5bd3d353f2616135abd39b2bd3dc78313e0
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-08-03 00:27:09 -0400

    update

--- dags/ray_scripts/script.py
+++ dags/ray_scripts/script.py
@@ -8,7 +8,7 @@ def hello_world():
 def main():
     ray.init()
     start_time = time.time()
-    duration = 15 * 60  # 15 minutes in seconds
+    duration = 2 * 60  # 15 minutes in seconds
 
     while time.time() - start_time < duration:
         result = ray.get(hello_world.remote())
